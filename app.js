d3c79a3f7d748991e550cde8823e80a67250a9bc
Nem Tudo <gf98289@gmail.com>
2026-08-13 01:30:49 -0300

    fix

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const path = require('path');
+const { execSync } = require('child_process');
 const fs = require('fs');
 const { app, BrowserWindow, ipcMain, shell, globalShortcut, Notification } = require('electron');
 
